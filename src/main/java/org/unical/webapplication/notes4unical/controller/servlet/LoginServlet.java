0d69e92c10a4842f6bd46daee38abb77ffbd934b
Nickbike03 <dtmncl03c24c349x@studenti.unical.it>
2025-02-06 12:05:53 +0100

    creazione del servlet login

---
+++ src/main/java/org/unical/webapplication/notes4unical/controller/servlet/LoginServlet.java
@@ -0,0 +1,27 @@
+package org.unical.webapplication.notes4unical.controller.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/doLogin")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        if (username.equals("prova1234@studeni.unical.it") && password.equals("1234") ){
+            req.getSession(true).setAttribute("username", username);
+            resp.sendRedirect("/");
+        }else{
+            RequestDispatcher dispatcher = req.getRequestDispatcher("views/loginErrato.html");
+            dispatcher.forward(req, resp);
+        }
+
+    }
+}
