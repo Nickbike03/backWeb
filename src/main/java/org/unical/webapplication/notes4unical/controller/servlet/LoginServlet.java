c1cdd2ed0313f39da57f1cc8e378bb8e55e2b43a
Nickbike03 <dtmncl03c24c349x@studenti.unical.it>
2025-02-07 11:50:09 +0100

    aggiunta del CrossOrigin

--- src/main/java/org/unical/webapplication/notes4unical/controller/servlet/LoginServlet.java
+++ src/main/java/org/unical/webapplication/notes4unical/controller/servlet/LoginServlet.java
@@ -2,6 +2,8 @@ package org.unical.webapplication.notes4unical.controller.servlet;
 
 import java.io.IOException;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,6 +11,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
+@CrossOrigin(origins = "http://localhost:4200")
 @WebServlet("/doLogin")
 public class LoginServlet extends HttpServlet {
     @Override
@@ -17,7 +21,7 @@ public class LoginServlet extends HttpServlet {
             System.out.println("Sono entrato nel controoler della registrazione");
             String username = req.getParameter("username");
             String password = req.getParameter("password");
-            if (username.equals("prova1234@studeni.unical.it") && password.equals("1234") ){
+            if (username.equals("prova1234@studenti.unical.it") && password.equals("1234") ){
                 req.getSession(true).setAttribute("username", username);
                 resp.sendRedirect("/");
             }else{
